f85a7840fadc9647b7952c6b0a8965a2212c531d
Shubhangi meshram <101330420+9689725619@users.noreply.github.com>
2022-04-16 14:35:31 +0530

    Add files via upload

---
+++ c++ using object pid ,char name.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class student{
+	private:
+		int a,b;
+		
+		public:
+			int add(int a,int b){
+				return a+b;
+			}
+			};
+			int main(int argc,char const*argv[])
+			{
+				student s;
+				int res=s,add(20,30);
+				cout<<"\n result is"<<res;
+				res=s.add(20,10);
+				cout<<"\n result is"<<res;
+				return 0;
+	}
+	
+		
